6668233301e7ca4e1c050daea7bebda784852a6e
Samm Zuest Cooper <samm@hfour.ca>
2019-11-05 01:50:32 -0800

    better video tests

--- test/video-test.js
+++ test/video-test.js
@@ -1,69 +1,104 @@
 require('../index').test( 'video-test', async function ( loopin ) {
 
-  // const filename = `video/vapourdude-h264.mp4`
-  const filename = `video/vapourdude-mjpeg.mov`
+  // const filename = `video/vapourdude-mjpeg.mov`
+  const filename = `video/vapourdude-h264.mp4`
+  // const filename = `video/vapourdude-iframes.mp4`
+  const seekTo = 3.2
+  const stepTo = 6
+  const scratchFrames = 60
 
+  await loopin.testSection( `Load ${filename}`, async () => {
+    loopin.patchYAML(`
+      video/test:
+        file: ${filename}
 
-  loopin.patchYAML(`
-    video/test:
-      file: ${filename}
+      show/buffer: test
+    `)
 
-    show/buffer: test
-  `)
- 
+    let event = await loopin.dispatchListen( 'video/test/' )
+    let result = await loopin.read('/video/test/')
+    loopin.log('result', { data: result } )
+    await loopin.testDelay(1000)
+  } )
 
+  await loopin.testSectionX( `Stop\ntimecode should be around 1:00`, async () => {
+    await loopin.patchYAML(`
+      video/test/clock/mode: stop
+    `)
+    await loopin.testDelay()
 
-  let event = await loopin.dispatchListen( 'video/test/' )
-  let result = await loopin.read('/video/test/')
+  } )
 
-  loopin.log('result', { data: result } )
-
-  await loopin.testDelay(1000)
-
-  await loopin.patchYAML(`
-    video/test/clock/mode: stop
-  `)
-
-  await loopin.testDelay()
-  loopin.log('clock', { data: await loopin.read('/video/test/clock') } )
-
-  await loopin.patchYAML(`
-    video/test/clock/time: 3
-  `)
-  await loopin.testDelay(500)
-
-  await loopin.patchYAML(`
-    video/test/clock/mode: frame
-  `)
-
-  await loopin.testDelay(500)
-
-  await loopin.patchYAML(`
-    video/test/clock:
-      mode: step
-      time: 5
-  `)
-
-  await loopin.dispatchListen('video/test/')
-  loopin.log('clock', { data: await loopin.read('/video/test/clock') } )
-
-  // await loopin.testDelay(500)
-
-  for ( let i = 0; i < 4; i ++ ) {
+  await loopin.testSectionX( `Seek to ${seekTo} seconds`, async () => {    
+    await loopin.patchYAML(`
+      video/test/clock/time: ${seekTo}
+    `)
     await loopin.testDelay(500)
-    await loopin.patch( true, 'video/test/clock/advance')
-  }
+  } )
 
-  await loopin.patchYAML(`
-    video/test/clock:
-      mode: stop
-  `)
+  await loopin.testSectionX( `Play in frame mode`, async () => {    
+    await loopin.patchYAML(`
+      video/test/clock/mode: frame
+    `)
 
-  for ( let i = 0; i < 600; i ++ ) {
-    await loopin.patch( { position: Math.random() }, 'video/test/clock/' )
-    // await loopin.testDelay(10)
+    await loopin.testDelay(1500)
+  } )
+
+  await loopin.testSection( `Creep to end and wait for event`, async () => {    
+    await loopin.patchYAML(`
+      video/test/clock: 
+        time: 9.3
+        mode: time
+        speed: 0.1
+    `)
+
+    await loopin.dispatchListen( 'sync:video/test/' )
+
+    let event = await loopin.dispatchListen( 'video/test/clock' )
+    loopin.log('event', { data: event } )
+
+    // await loopin.testDelay(50000)
+
+    // await loopin.patchYAML(`
+    //   video/test/clock/mode: frame
+    // `)
+
+    // await loopin.testDelay(500)
+  } )
+
+
+
+  await loopin.testSection( `Seek to ${stepTo} and set step mode`, async () => {    
+    await loopin.patchYAML(`
+      video/test/clock:
+        mode: step
+        time: ${stepTo}
+    `)
     await loopin.dispatchListen('video/test/')
-  }
+
+    await loopin.testDelay(500)
+
+    for ( let i = 0; i < 30; i ++ ) {
+      await loopin.testDelay(200)
+      await loopin.patch( true, 'video/test/clock/advance')
+    }
+    await loopin.testDelay(600)
+
+  } )
+
+
+  await loopin.testSection( `${scratchFrames} frames of random scratching`, async () => {   
+    await loopin.patchYAML(`
+      video/test/clock:
+        mode: stop
+    `)
+
+    for ( let i = 0; i < scratchFrames; i ++ ) {
+      await loopin.patch( { position: Math.random() }, 'video/test/clock/' )
+      // await loopin.testDelay(10)
+      await loopin.dispatchListen('video/test/')
+    }
+  } )
 
   
 })
--- test/watchFile-test.js
+++ test/watchFile-test.js
@@ -14,31 +14,42 @@ require('../index').test( 'watchFile', async function ( loopin ) {
   assert.isObject( watcher )
   assert.isFunction( watcher.on )
 
-  let preset = {
-    'uniform/float/foo': Math.random()
-  }
+  let preset
+
+  await fs.unlink( file )
+
+  // promise = new Promise( async ( resolve ) => {
+  //   watcher.on('change', resolve )
+  //   await writePreset()
+  // } )
+  // await promise.timeout( 1000 )
+  // await loopin.testDelay()
+
+  // promise = new Promise( async ( resolve ) => {
+  //   watcher.on('load', function( result ) {
+  //     assert.equal( result.type, 'preset')
+  //     resolve()
+  //   } )
+  //   await writePreset()
+  // } )
+  // await promise.timeout( 1000 )
+  // await loopin.testDelay()
+  await loopin.testDelay(10000)
 
   promise = new Promise( async ( resolve ) => {
-    watcher.on('change', resolve )
-    await writePreset()
-  } )
-  await promise.timeout( 1000 )
-  await loopin.testDelay()
-
-  promise = new Promise( async ( resolve ) => {
-    watcher.on('load', function( result ) {
-      assert.equal( result.type, 'preset')
+    loopin.on('patch', function( result ) {
+      assert.deepEqual( result.value, preset )
       resolve()
     } )
     await writePreset()
   } )
-  await promise.timeout( 1000 )
+  await promise.timeout( 10000 )
   await loopin.testDelay()
 
 
 
-
   async function writePreset() {
+    preset = { uniform: { float: { foo: Math.random() } } }
     await fs.outputFile( loopin.filesResolve(file), YAML.dump( preset ) )
   }
 } )
